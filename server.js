87bdae2280a1b57a2198865787c0465c815b6d3b
KalGOV <govindaadithya.rajagopalan@gmail.com>
2016-10-10 21:01:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/home', function (req, res) {
+    res.send('Idhu dhan veedu :)');
+});
+
+app.get('/office', function (req, res) {
+    res.send('Idhu dhan abeesu');
+});
+
+app.get('/room', function (req, res) {
+    res.send('poi aki adi :P');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
